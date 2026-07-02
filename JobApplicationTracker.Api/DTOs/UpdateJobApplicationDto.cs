a5e603170795871dcc4246a809f87eb9ef3b5df2
Uğur Şişman <ugursismanbusiness@gmail.com>
2026-07-02 04:45:23 +0300

    Add validation for job application DTOs

--- JobApplicationTracker.Api/DTOs/UpdateJobApplicationDto.cs
+++ JobApplicationTracker.Api/DTOs/UpdateJobApplicationDto.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JobApplicationTracker.Api.DTOs;
 
 public class UpdateJobApplicationDto
 {
+    [Required]
+    [MaxLength(100)]
     public string Company { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
     public string Position { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(50)]
     public string Status { get; set; } = string.Empty;
 }
